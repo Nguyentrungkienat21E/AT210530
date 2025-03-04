5ef7bf2672223858c995262f5e52738e5213f2fe
Nguyentrungkienat21E <ntk27072006@gmail.com>
2025-03-04 09:07:18 +0700

    Add files via upload

--- ltcb-bai8.c
+++ ltcb-bai8.c
@@ -1,39 +1,34 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-	int a,b,c,chuVi;
-	float dienTich;
+	float a,b,c,dienTich,p;
 	
-	printf("Nhap gia tri cua ban : ");
-	scanf("%d%d%d",&a,&b,&c);
+	printf("Nhap vao gia tri cua a,b,c : ");
+	scanf("%f%f%f",&a,&b,&c);
 	
-	chuVi = a + b + c ;
-	float p = chuVi / 2;
-	dienTich = sqrt(p*(p-a)*(p-b)*(p-c));
-	
-	if(a + b > c || a + c > b || b + c > a){
-		printf("a,b,c la ba canh cua mot tam giac \n");
-	    printf("Chu vi cua tam giac la %.2f . Dien tich cua tam giac la %.2f \n",chuVi,dienTich);
-		
-		if(a*a = b*b + c*c || b*b = a*a + c*c || c*c = a*a + b*b){
-		    printf("Tam giac do la tam giac vuong ");
-		}
-		else if(a*a = b*b + c*c && b == c ||b*b = a*a + c*c && a == c ||c*c = a*a + b*b && a == b){
-			printf("Tam giac do la tam giac vuong can ");
+	if(a + b > c && a + c > b && b + c > a){
+		p =( a + b + c ) / 2;
+	    dienTich = sqrt(p * (p - a) * (p - b) * (p - c));
+		if(a==b || a==c || b==c){
+			printf("Ba so tao thanh tam giac can va co chu vi = %.2f , dien tich = %.2f",p *2,dienTich);
 		}
 		else if(a==b && a==c && b==c){
-			printf("Tam giac do la tam giac deu");
+			printf("Ba so tao thanh tam giac deu va co chu vi = %.2f , dien tich = %.2f",p *2,dienTich);	
 		}
-		else if(a==b || a==c || b==c){
-			printf("Tam giac do la tam giac can");
+		else if(a * a == b * b+ c *c || b * b == a * a+c * c || c * c == a * a+b * b){
+			printf("Ba so tao thanh tam giac vuong va co chu vi = %.2f , dien tich = %.2f",p *2,dienTich);
+		}
+		else if(a * a == b * b+c * c && b==c || b * b == a * a+c * c && a==c || c * c == a * a+b * b && a==b){
+			printf("Ba so tao thanh tam giac vuong can va co chu vi = %.2f , dien tich = %.2f",p *2,dienTich);
 		}
 		else{
-			printf("Tam giac do la tam giac thuong ");
-		}
+			printf("Ba so tao thanh tam giac thuong va co chu vi = %.2f , dien tich = %.2f",p *2,dienTich);
 		}
 	}
 	else{
-		printf("a,b,c khong phai la canh cua tam giac");
+		printf("Ba so nhap vao khong tao thanh tam giac");
 	}
+
 	
 }
